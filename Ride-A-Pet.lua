6150ce6a4ba0ecce51a557f22ae8fb1a375524ce
LeThanhDuy <duyl28093@gmail.com>
2026-09-24 17:59:10 +0700

    Update Ride-A-Pet.lua

--- Ride-A-Pet.lua
+++ Ride-A-Pet.lua
@@ -1,3 +1,3 @@
 -- The File Obfuscate By Gay Obfuscate V1.3
 
-loadstring(game:HttpGet("https://raw-load-shield.lovable.app/api/public/raw/acd0ca5ad6510da863156e979fbd4a01"))()
+loadstring(game:HttpGet("https://thanhduyhubxyz.lovable.app/api/public/raw/acd0ca5ad6510da863156e979fbd4a01"))()
